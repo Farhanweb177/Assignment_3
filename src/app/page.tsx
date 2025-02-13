2c5a3c03884f47c359bc24fa7125660d7d951b14
Farhanweb177 <160813201+Farhanweb177@users.noreply.github.com>
2025-02-14 03:24:12 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+import Footer from "./components/Footer/Footer";
+
+
+
+export default function Home() {
+  return (
+    <div className="bg-zinc-400">
+      <Header></Header>
+
+      <Hero></Hero>
+       
+      <p>
+        Welcome to my portfolio! This home page is a showcase of my skills,
+        projects, and experiences as a web developer, all powered by Next.js.
+        Here, you will get an insight into my journey in the tech world, from the
+        innovative projects I have built to the technical expertise I have developed
+        along the way. Through this portfolio, I aim to demonstrate my passion
+        for creating dynamic and responsive web applications using modern
+        technologies. Feel free to explore and reach out via the contact section
+        if you have any questions or opportunities you did like to discuss. 🚀
+        What is next on our coding adventure?
+      </p>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+
